eeb82d0e11023b88badac367dd4ba5e2d8bb3f64
Zxtsv <bernardo_pprz@comunidad.unam.mx>
2022-02-11 16:08:24 -0600

    Add function invitanos un café

---
+++ js/blockchain.js
@@ -0,0 +1,16 @@
+const donationButtom = document.querySelector(".Donate");
+
+donationButtom.addEventListener("click", () => {
+  getAccountDonate();
+});
+
+function getAccountDonate() {
+  Swal.fire({
+    title: "¡Dona Bitcoin con Bitso!",
+    text: "Abre la app de Bitso y escanea el código",
+    imageUrl: "../images/CodigoDonacion.png",
+    imageWidth: 200,
+    imageHeight: 200,
+    imageAlt: "Código donación",
+  });
+}
